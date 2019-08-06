c9190b371bdb9bf67d04f5d388134d637edf055c
Nelly Shishkova <45102217+Busiky@users.noreply.github.com>
2019-08-06 09:09:26 +0300

    Update russian_flag.py
    Нарисуем личико

--- russian_flag.py
+++ russian_flag.py
@@ -13,6 +13,7 @@ def draw():
     pygame.draw.rect(screen, pygame.Color('white'), (50, 20, 430, 100))
     pygame.draw.rect(screen, pygame.Color('red'), (50, 220, 430, 100))
     pygame.draw.rect(screen, pygame.Color('blue'), (50, 120, 430, 100))
+    # draw smile
 
 draw()
 
