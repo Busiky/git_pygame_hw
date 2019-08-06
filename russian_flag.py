f108e21876a684c9512adf1fd9404cf51766b1b4
Busiky <ya.busiky@gmail.com>
2019-08-06 09:11:36 +0300

    Рожица

--- russian_flag.py
+++ russian_flag.py
@@ -14,6 +14,7 @@ def draw():
     pygame.draw.rect(screen, pygame.Color('red'), (50, 220, 430, 100))
     pygame.draw.rect(screen, pygame.Color('blue'), (50, 120, 430, 100))
     # draw smile
+    pygame.draw.ellipse(screen, pygame.Color('yellow'), (400, 400, 80, 80))
 
 draw()
 
